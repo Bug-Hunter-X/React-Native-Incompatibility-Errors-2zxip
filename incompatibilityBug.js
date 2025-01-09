58ce50b8c3e5138c303620e622917ce9007f1453
BugHunter-X <askzenai@gmail.com>
2025-01-09 16:50:07 +0100

    Add incompatibilityBug.js file

---
+++ incompatibilityBug.js
@@ -0,0 +1,1 @@
+This error occurs when using a library that is not compatible with the version of React Native you are using. For example, if you are using React Native 0.64 and a library that only supports up to 0.63, you will get this error.  Another common cause is attempting to use features that your RN version doesn't support, like newer hooks or components.
